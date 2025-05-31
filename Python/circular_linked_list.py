94041c4a2afd0f48fa722173918b74c5aad098f8
Codyaxe <vincelorzano@gmail.com>
2025-05-31 20:17:16 +0800

    implementation done

--- Python/circular_linked_list.py
+++ Python/circular_linked_list.py
@@ -9,8 +9,22 @@ class Linked_List():
         self.head = None
         self.size = 0
 
-    def insert(self):
-        pass
+    def insert(self, value, index):
+        current = self.head
+        if index == 0:
+            self.prepend(value)
+            return
+        if index == self.size:
+            self.append(value)
+            return
+        if index > self.size - 1:
+            print("Index range out of bounds!")
+        else:
+            for i in range(index - 1):
+                current = current.next
+            new_node = Node(value, current.next)
+            current.next = new_node
+            self.size += 1
 
     def append(self, value):
         current = self.head
@@ -20,7 +34,7 @@ class Linked_List():
             new_node.next = self.head
             self.size += 1
         else:
-            current = self.traverse()
+            current = self.traverse(current)
             new_node = Node(value, self.head)
             current.next = new_node
             self.size += 1
@@ -33,20 +47,66 @@ class Linked_List():
             new_node.next = self.head
             self.size += 1
         else:
-            new_node = Node(value, current)
+            current = self.traverse(current)
+            new_node = Node(value, self.head)
+            current.next = new_node
             self.head = new_node
-            current = self.traverse()
-            current.next = self.head
             self.size += 1
 
-
-    def delete(self):
-        pass
+    def delete(self, index):
+        current = self.head
+        if index == 0:
+            self.head = current.next
+            self.size -= 1
+            return
+        if index > self.size - 1:
+            print("Index range out of bounds!")
+        else:
+            for i in range(index - 1):
+                current = current.next
+            if current.next is not self.head:
+                current.next = current.next.next
+                self.size -= 1
+            else:
+                current.next = self.head
+                self.size -= 1
 
     def traverse(self, pointer):
-        while pointer.next is not None:
+        while pointer.next is not self.head:
             pointer = pointer.next
         return pointer
     
-    def search(self):
-        pass
+    def search(self, value):
+        current = self.head
+        for i in range(self.size):
+            if current.value == value:
+                print(f"{value} is at index {i}")
+                return
+            else:
+                current = current.next
+        print(f"{value} cannot be found!")
+
+    def print(self):
+        current = self.head
+        for i in range(self.size):
+            print(f"{current.value} -> ", end = "")
+            current = current.next
+        print(current.value, "< HEAD")
+
+test = Linked_List()
+test.append(2)
+test.append(4)
+test.append(5)
+test.print()
+test.delete(2)
+test.print()
+test.prepend(8)
+test.print()
+test.prepend(9)
+test.print()
+test.search(4)
+test.insert(20, 1)
+test.print()
+test.insert(20, 4)
+test.insert(99, 6)
+test.print()
